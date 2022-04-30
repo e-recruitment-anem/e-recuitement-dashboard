2b3e9ae1c818ff898d5fce8a9d0a3e2a8a34496f
Abdelouadoud8 <a.mahdaoui@esi-sba.dz>
2022-05-01 00:51:20 +0100

    🎨 Improved project structure, 💄 Added OfferDetails page

---
+++ src/components/demandeur/SimilarOffers/SimilarOffers.tsx
@@ -0,0 +1,53 @@
+import { Box, InputProps } from "@chakra-ui/react";
+import { FC } from "react";
+import SkillTag from "../SkillTag/SkillTag";
+import "./SimilarOffers.scss";
+
+interface Props extends InputProps {
+  role?: string;
+  description?: string;
+  agence?: string;
+  deadline?: string;
+  cover?: string;
+  agenceImage?: string;
+  applicants?: string;
+}
+
+const SimilarOffers: FC<Props> = ({
+  role,
+  description,
+  agence,
+  deadline,
+  applicants,
+  cover,
+  agenceImage,
+}) => {
+  return (
+    <Box className="similaroffers">
+      <div className="similaroffers--heading">
+        <img src={cover} alt="" className="similaroffers--heading-cover" />
+        <div className="similaroffers--heading-text">
+          <h1>{role}</h1>
+          <p>{description}</p>
+        </div>
+      </div>
+      <div className="similaroffers--middle">
+        <SkillTag content="Figma" />
+        <SkillTag content="React Js" />
+        <SkillTag content="Figma" />
+      </div>
+      <div className="similaroffers--bottom">
+        <div className="similaroffers--bottom-agence">
+          <img src={agenceImage} alt="" />
+          <span>{agence}</span>
+        </div>
+        <div className="similaroffers--bottom-applicants">
+          <span>{deadline}</span>
+          <span>{applicants}</span>
+        </div>
+      </div>
+    </Box>
+  );
+};
+
+export default SimilarOffers;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from "react";
 
 // Hello components
-import Hello from './components/Hello';
+import Hello from "./components/Hello";
+import OfferDetails from "./views/demandeur/OfferDetails";
 
 const routes = [
   {
     path: "/",
     element: <Hello />,
+  },
+  {
+    path: "/offerdetails",
+    element: <OfferDetails />,
   },
 ];
 
