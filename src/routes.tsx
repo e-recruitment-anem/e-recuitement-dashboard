568f4376b6e47192a323496d6d73675be0c98be6
Neoxs <m.abdelkaderkharoubi@esi-sba.dz>
2022-05-18 01:55:41 +0100

    Fix merge conflict

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 
 // Hello components
-<<<<<<< HEAD
 import Hello from './components/Hello';
 import SignIn from './views/auth/SignIn';
 import Dashboard from './views/dashboard/Dashboard';
+import Home from './views/demandeur/Home';
 import OfferDetails from './views/demandeur/OfferDetails';
-=======
-import Hello from "./components/Hello";
-import Home from "./views/demandeur/Home";
-import OfferDetails from "./views/demandeur/OfferDetails";
->>>>>>> 0b226bf13edc2b4920454538b08f827a9c815c65
 
 const routes = [
   {
